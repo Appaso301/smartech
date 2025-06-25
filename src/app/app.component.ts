62da149c150c08df86bd139a0080afc736bd4c7e
Vivekhake <162538476+Vivekhake@users.noreply.github.com>
2025-06-25 15:58:58 +0530

    code_rechanges

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,22 @@
-import { Component } from "@angular/core";
-import { RouterOutlet } from "@angular/router";
-import { HeaderComponent } from "./header/header.component";
-import { NavbarComponent } from "./navbar/navbar.component";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
 
 @Component({
-  selector: "app-root",
-  imports: [RouterOutlet, HeaderComponent, HomeComponent, NavbarComponent, AboutComponent],
-  templateUrl: "./app.component.html",
-  styleUrl: "./app.component.scss",
+  selector: 'app-root',
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    HomeComponent,
+    NavbarComponent,
+    AboutComponent,
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = "smartech";
+  title = 'smartech';
 }
