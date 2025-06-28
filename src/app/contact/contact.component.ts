97349f3bd02248064136bad90d001ca2ce09a774
prashant-chavan-2004 <prashant21102004@gmail.com>
2025-06-28 18:37:55 +0530

    portfolio changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -7,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
+clickCount = 0;
+clickTimer: any;
+
+onAddressClick() {
+  this.clickCount++;
+
+  if (this.clickCount === 2) {
+    this.clickCount = 0;
+
+    const mapsUrl = 'https://www.google.com/maps/search/?api=1&query=Flat+No+C2+47,+Kendriya+Vihar,+Moshi+Pradhikaran,+Sector+No+4,+Pune,+Maharashtra+412105,+India';
+    window.open(mapsUrl, '_blank');
+  } else {
+    this.clickTimer = setTimeout(() => {
+      this.clickCount = 0;
+    }, 400);
+  }
+}
 
 }
