bfb8acf11047767095242b67d7dcd79258a5846c
prashant-chavan-2004 <prashant21102004@gmail.com>
2025-07-09 13:18:31 +0530

    contact-navbar changes

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -18,7 +18,7 @@ export class ContactComponent {
       this.clickCount = 0;
 
       const mapsUrl =
-        'https://www.google.com/maps/place/C+2+kendriya+vihar/@18.649465,73.8475504,206m/data=!3m1!1e3!4m14!1m7!3m6!1s0x3bc2c70d31f8880b:0xe682fdb9e362f03d!2sC+2+kendriya+vihar!8m2!3d18.6494848!4d73.8481303!16s%2Fg%2F11t2t_rr9m!3m5!1s0x3bc2c70d31f8880b:0xe682fdb9e362f03d!8m2!3d18.6494848!4d73.8481303!16s%2Fg%2F11t2t_rr9m?entry=ttu&g_ep=EgoyMDI1MDYzMC4wIKXMDSoASAFQAw%3D%3D';
+        'https://www.google.com/maps/place/Tulsi+Pratham+Society+Sector+06/@18.6499894,73.8425948,124m/data=!3m1!1e3!4m6!3m5!1s0x3bc2b80a1ef1cf27:0x5a375eb6fa789d4!8m2!3d18.6499982!4d73.8425262!16s%2Fg%2F11b75m4pvr?entry=ttu&g_ep=EgoyMDI1MDcwNi4wIKXMDSoASAFQAw%3D%3D';
       window.open(mapsUrl, '_blank');
     } else {
       this.clickTimer = setTimeout(() => {
